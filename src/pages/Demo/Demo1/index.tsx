63f0e524dfe3a3242a1f11685f40ba094b7570ba
kivet <wanghuixiang@druid.tech>
2022-01-27 14:39:18 +0800

    feat: 新增css类型校验规则

--- src/pages/Demo/Demo1/index.tsx
+++ src/pages/Demo/Demo1/index.tsx
@@ -7,7 +7,13 @@ import styles from './index.less';
 interface IProps {}
 
 const Demo1: FC<IProps> = () => {
-  return <div className={styles.container}>Demo1</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>Demo1 title</div>
+      <div className={styles.content}>Demo1 content</div>
+      <div className={styles.footer}>Demo1 footer</div>
+    </div>
+  );
 };
 
 export default Demo1;
